5b6f87ff0bf6bc73ebffbc544ae6c4baf0f8092c
Tim Keller <timothy.m.keller@gmail.com>
2021-01-18 16:51:08 -0800

    Added more test data to JSON file. Added functioning delete routing and test code for POST.

--- start.js
+++ start.js
@@ -15,7 +15,39 @@ app.get( '/notes', (req, res) => {
         res.end( data );
     })
 })
-
+// Delivers the JSON file on GET request
+app.get( '/api/notes', ( req, res ) => {
+    res.sendFile( __dirname + "/db/db.json", "utf8", function( err, data ) {
+        if ( err ) throw err;
+        res.end( data );
+    })
+})
+// // TODO: Append a new note with a unique ID to the end of the JSON file.
+// app.post( '/api/notes', function( req, res ) {
+//     console.log( "Post request" );
+//     console.log( req.body );
+//     fs.readFile( __dirname + "/db/db.json", "utf8", function( err, data ) {
+//         let notes = JSON.parse( data );
+//         notes = JSON.stringify( notes );
+//         fs.writeFile( __dirname + "/db/db.json", notes, function() {
+//             if ( err ) throw err;
+//         })
+//     })
+//     res.end();
+// });
+// Reads the JSON file, deletes the chosen ID, then writes the modified file back.
+app.delete( '/api/notes/:id', function( req, res ) {
+    let toDelete = req.params.id;
+    fs.readFile( __dirname + "/db/db.json", "utf8", function( err, data ) {
+        let notes = JSON.parse( data );
+        let stringified = JSON.stringify( notes.filter( note => note.id != toDelete ));
+        fs.writeFile( __dirname + "/db/db.json", stringified, function() {
+            if ( err ) throw err;
+        })
+    })
+    res.end();
+});
+// Defaults to serving the homepage.
 app.get( '*', (req, res) => {
     fs.readFile( __dirname + "/public/index.html", function( err, data ) {
         if ( err ) throw err;
